829805dcf33d2d0fec1890a685fa04837e2002ce
one-some <onesomegit@outlook.com>
2025-08-11 20:45:00 -0500

    ZZ

--- worldren/chunk.gd
+++ worldren/chunk.gd
@@ -27,7 +27,7 @@ func generate() -> void:
 				var val = noise.get_noise_3dv(global_pos)
 				var idx = data.get_index(local_pos)
 				
-				data.density[idx] = val - (global_pos.y / 10.0)
+				data.density[idx] = val - (global_pos.y / 20.0)
 				
 				var mat = 2 # Stone
 				if global_pos.y > -3.0:
@@ -41,6 +41,7 @@ func generate() -> void:
 func generate_mesh() -> void:
 	var st = SurfaceTool.new()
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	st.set_custom_format(0, SurfaceTool.CUSTOM_RGBA_FLOAT)
 	
 	for x in ChunkData.CHUNK_SIZE:
 		for y in ChunkData.CHUNK_SIZE:
@@ -72,8 +73,7 @@ func generate_mesh() -> void:
 				
 				var edge_mask = MarchData.edge_table[index]
 				var cell_vertices = []
-				var cell_colors = []
-				var cell_uv2s = []
+				var vertex_data_map = []
 				var vertex_map = []
 				vertex_map.resize(12)
 				vertex_map.fill(-1)
@@ -81,27 +81,14 @@ func generate_mesh() -> void:
 				for i in range(12):
 					if not (edge_mask & (1 << i)): continue
 					var vertex_indices = [
-						[0, 1],
-						[1, 2],
-						[2, 3],
-						[3, 0],
-						[4, 5],
-						[5, 6],
-						[6, 7],
-						[7, 4],
-						[0, 4],
-						[1, 5],
-						[2, 6],
-						[3, 7]
+						[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6],
+						[6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]
 					][i]
 					
 					var p0 = corner_positions[vertex_indices[0]]
 					var p1 = corner_positions[vertex_indices[1]]
 					var d0 = corner_densities[vertex_indices[0]]
 					var d1 = corner_densities[vertex_indices[1]]
-					var m0 = corner_materials[vertex_indices[0]]
-					var m1 = corner_materials[vertex_indices[1]]
-					
 					
 					var t = 0.5
 					if d1 - d0:
@@ -109,8 +96,49 @@ func generate_mesh() -> void:
 						
 					var vertex_pos = p0.lerp(p1, t)
 					cell_vertices.append(vertex_pos)
-					cell_colors.append(Color(m0 / 255.0, m1 / 255.0, 0.0))
-					cell_uv2s.append(Vector2(t, 0.0))
+					
+					var rel_pos = vertex_pos - Vector3(x, y, z)
+					
+					var weights = [
+						(1.0 - rel_pos.x) * (1.0 - rel_pos.y) * (1.0 - rel_pos.z),
+						rel_pos.x * (1.0 - rel_pos.y) * (1.0 - rel_pos.z),
+						rel_pos.x * (1.0 - rel_pos.y) * rel_pos.z,
+						(1.0 - rel_pos.x) * (1.0 - rel_pos.y) * rel_pos.z,
+						(1.0 - rel_pos.x) * rel_pos.y * (1.0 - rel_pos.z),
+						rel_pos.x * rel_pos.y * (1.0 - rel_pos.z),
+						rel_pos.x * rel_pos.y * rel_pos.z,
+						(1.0 - rel_pos.x) * rel_pos.y * rel_pos.z,
+					]
+					
+					var mat_weights = {}
+					for j in range(8):
+						var mat_id = corner_materials[j]
+						if mat_id not in mat_weights:
+							mat_weights[mat_id] = 0.0
+						mat_weights[mat_id] += weights[j]
+					
+					var sorted_mats = mat_weights.keys()
+					sorted_mats.sort_custom(func(a, b): return mat_weights[a] > mat_weights[b])
+					
+					var final_ids = Vector4.ZERO
+					var final_weights = Vector4.ZERO
+					var total_weight = 0.0
+					
+					var num_mats = min(sorted_mats.size(), 4)
+					for j in range(num_mats):
+						var mat_id = sorted_mats[j]
+						var weight = mat_weights[mat_id]
+						final_ids[j] = float(mat_id)
+						final_weights[j] = weight
+						total_weight += weight
+					
+					if total_weight > 0.0:
+						final_weights /= total_weight
+					
+					vertex_data_map.append({
+						"ids": final_ids,
+						"weights": final_weights
+					})
 					
 					vertex_map[i] = cell_vertices.size() - 1
 				
@@ -118,9 +146,13 @@ func generate_mesh() -> void:
 				var i = 0
 				while triangles[i] != -1:
 					for idx in [i, i + 2, i + 1]:
-						st.set_color(cell_colors[vertex_map[triangles[idx]]])
-						st.set_uv2(cell_uv2s[vertex_map[triangles[idx]]])
-						st.add_vertex(cell_vertices[vertex_map[triangles[idx]]])
+						var vertex_index_in_cell = vertex_map[triangles[idx]]
+						var data = vertex_data_map[vertex_index_in_cell]
+						
+						st.set_color(Color(data.weights.x, data.weights.y, data.weights.z, data.weights.w))
+						st.set_custom(0, Color(data.ids.x, data.ids.y, data.ids.z, data.ids.w))
+						st.add_vertex(cell_vertices[vertex_index_in_cell])
 					i += 3
 	st.generate_normals()
+	#st.generate_tangents()
 	self.mesh = st.commit()
